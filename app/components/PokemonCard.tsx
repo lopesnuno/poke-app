bf9868284fd1ffc03b298c9566ef9f08ad0778b2
lopesnuno <lopesnuno@ipvc.pt>
2024-03-14 10:24:09 +0000

    Removed use of useNavigate

--- app/components/PokemonCard.tsx
+++ app/components/PokemonCard.tsx
@@ -2,16 +2,6 @@ import { useNavigate } from 'react-router';
 import Button from '~/components/Button';
 
 export default function PokemonCard(pokemon: { id: number, name: string, sprite: string }) {
-  const navigate = useNavigate();
-
-  function handlePrevious() {
-    navigate(`/${pokemon.id - 1}`);
-  }
-
-  function handleNext() {
-    navigate(`/${pokemon.id + 1}`);
-  }
-
   return (
     <div className='flex flex-col items-center'>
       <div className='flex flex-col items-center border-2 border-gray-500 rounded p-4 gap-y-4 bg-yellow-500 w-[210px]'>
@@ -26,8 +16,8 @@ export default function PokemonCard(pokemon: { id: number, name: string, sprite:
 
         <form method='post'>
           <div className='flex flex-row gap-x-2'>
-            {pokemon.id !== 1 && (<Button name='action' value='previous' onClick={handlePrevious}>Previous</Button>)}
-            <Button name='action' value='next' onClick={handleNext}>Next</Button>
+            {pokemon.id !== 1 && (<Button name='action' value='previous'>Previous</Button>)}
+            <Button name='action' value='next'>Next</Button>
             <input name='pokemon-id' type='hidden' value={pokemon.id} />
           </div>
         </form>
